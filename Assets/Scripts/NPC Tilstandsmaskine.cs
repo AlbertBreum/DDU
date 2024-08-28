e76e2a53309976c341d6a169dd55633caa6fafd8
Asger58 <asgerfonager@gmail.com>
2024-08-28 11:16:07 +0200

    pathing 1.1 not working

--- Assets/Scripts/NPC Tilstandsmaskine.cs
+++ Assets/Scripts/NPC Tilstandsmaskine.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Transform player;
+    private NavMeshAgent agent;
+    private float stoppingDistance = 0.3f;
+    public List<Transform> points = new List<Transform>();
+    private int posIndex = 0;
+
+    bool IsWorking = false;
+    bool IsFree = false;
+    bool IsAsleep = false;
+    bool Isinfected = false;
     // Start is called before the first frame update
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Invoke(nameof(moveOnTime), 4f);
+    }
+    
     public enum Activity
     {
         Work,
@@ -33,13 +55,37 @@ public class NewBehaviourScript : MonoBehaviour
 
         }
     }
+    
     void Work()
     {
         // her skal NPC'erne kunne finde deres vej til arbejde
+        if(IsWorking == false)
+        {
+            IsWorking = true;
+            moveOnTime();
+            
+        }
+    }
+    void moveOnTime()
+    {
+        Debug.Log("hello");
+        if (IsWorking == true)
+        {
+            Debug.Log(points[posIndex]);
+            agent.SetDestination(points[posIndex].position);
+            agent.isStopped = false;
+            Invoke(nameof(moveOnTime), 4f); //call again in 4 seconds
+            posIndex = (posIndex + 1) % (points.Count);
+        }
     }
     void Free()
     {
+        if(IsFree == true)
+        {
+            Debug.Log("Off work");
+        }
         // Her skal NPC'erne kunne finde fra deres arbejde hen til at sted hvor de kan slappe af
+        
     }
     void Asleep()
     {
@@ -47,6 +93,6 @@ public class NewBehaviourScript : MonoBehaviour
     }
     void Infected()
     {
-        // Her skal NPC'en forsætte sin normale hverdag men md mulighed fo rat smitte andre omkring sig
+        // Her skal NPC'en forsætte sin normale hverdag men med mulighed for´at smitte andre omkring sig
     }
 }
