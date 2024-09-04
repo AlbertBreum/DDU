7343f9a89c240c11b02b7222838c4407beee783d
Asger58 <asgerfonager@gmail.com>
2024-09-04 08:50:35 +0200

    fixes totalt lavet af os uden hjælp fra en Albert

--- Assets/Scripts/Covid.cs
+++ Assets/Scripts/Covid.cs
@@ -6,64 +6,22 @@ using UnityEngine;
 public class Covid : MonoBehaviour
 {
     
-    private List<Human> nearbyHumans = new List<Human>(); //Opretter en liste til at holde styr på mennesker indenfor smitteradius
-    public float radiusOfInfection;
+   
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        GameObject g1 = new GameObject("g1");
+       GameObject g1 = new GameObject("g1");
         Human human = new Human();
         g1.AddComponent<Human>();
 
         g1.GetComponent<Human>().Age = 10;
     }
 
-    private void OnTriggerEnter(Collider other) //Detekterer, at et menneske kommer indenfor en given radius
-    {
-        Debug.Log("enter");
-        if (other.gameObject.tag == "human")
-        {
-            nearbyHumans.Add(other.Human);
-        }
-    }
+  
 
-    private void OnTriggerExit(Collider other) //Detekterer, at et menneske kommer udenfor en given radius
-    {
-        if (other.gameObject.tag.Equals("human"))
-        {
-            nearbyHumans.Remove(other.gameObject);
-        }
-    }
-
-    void InfectOthers()
-    {
-        float InfectionChance(float r) {
-            float risk;
-            risk = Mathf.Exp(-Mathf.Pow(r, 2)/radiusOfInfection);
-            return risk;
-
-        }
-
-        foreach (var human in nearbyHumans) 
-        {
-            Debug.Log(human);
-            Debug.Log("Hello");
-            float num = Random.Range(0f, 1f);
-            float r = 5.0f;
-            if (num > InfectionChance(r))
-            {
-                human.activeDisease = Human.Disease.Covid;
-                
-            }
-
-
-
-        }
-
-
-
-    }
+    
     
     
     
