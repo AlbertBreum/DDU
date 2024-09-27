2374de72e949520fb4f833a99a525b2e5333cd96
AlbertBreum <142424136+AlbertBreum@users.noreply.github.com>
2024-09-27 09:01:14 +0200

    changed to plague script

--- Assets/Scripts/Plague.cs
+++ Assets/Scripts/Plague.cs
@@ -2,25 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Plague : MonoBehaviour
 {
+    public int radiusOfInfection = 2;
     private Human infectedHuman;
     private float plagueTime;
     ParticleSystem plagueParticles;
+    public List<Human> nearbyHumans;
 
     // Start is called before the first frame update
     void Start()
     {
+        nearbyHumans = GetComponent<Human>().nearbyHumans;
         plagueParticles = GetComponent<Human>().Plague;
         plagueParticles.Play();
 
     }
 
+    void InfectOthers() //Infekterer med pesten
+    {
+        float InfectionChance(float r)
+        {
+            float risk;
+            risk = Mathf.Exp(-Mathf.Pow(r, 2) / radiusOfInfection);
+            return risk;
+        }
+
+        foreach (Human human in nearbyHumans)
+        {
+            float num = Random.Range(0f, 1f);
+            float r = 5.0f;
+            if (num > InfectionChance(r))
+            {
+                //human.activeDisease = anyDisease;
+                //Plague sc = gameObject.AddComponent(typeof(Plague)) as Plague;
+                Debug.Log("Human " + human + " is infected!");
+                Plague p = human.AddComponent<Plague>();
+            }
+        }
+    }
+
+
+
+
     // Update is called once per frame
     void Update()
     {
         plagueTime += Time.deltaTime;
+        InfectOthers();
         CureDisease();
         Die();
     }
@@ -31,8 +62,8 @@ public class Plague : MonoBehaviour
         if (n <= 5*plagueTime)
         {
             //plagueParticles.Stop();
-            infectedHuman.activeDisease = Disease.None;
-            infectedHuman.currentState = State.Incubation;
+            //infectedHuman.activeDisease = Disease.None;
+            //infectedHuman.currentState = State.Incubation;
             
         }
     }
